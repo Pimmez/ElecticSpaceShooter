cff8d8203d92f07d0f79fe99d405f6ecfb041e4b
PCZolder\Pimmez <pimhoreman@gmail.com>
2018-05-31 21:46:45 +0200

    BASE START: -Added Prefab Player -Added LevelProgress -Added Boundary Script -Added PlayerController Script -Added Background Texture -Added material

---
+++ ElectricSpaceShooter/Assets/Scripts/PlayerBoundary.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerBoundary : MonoBehaviour {
+
+	public static PlayerBoundary Instance { get { return GetInstance(); } }
+
+	#region Singleton
+	private static PlayerBoundary instance;
+
+	private static PlayerBoundary GetInstance()
+	{
+		if (instance == null)
+		{
+			instance = FindObjectOfType<PlayerBoundary>();
+		}
+		return instance;
+	}
+	#endregion
+
+	[SerializeField] private float xMin, xMax, zMin, zMax;
+
+	public void BoundaryBox(Rigidbody rigid)
+	{
+		rigid.position = new Vector3(Mathf.Clamp(rigid.position.x, xMin, xMax), 0f, Mathf.Clamp(rigid.position.z, zMin, zMax));
+	}
+}
---
+++ ElectricSpaceShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System;
+
+public class PlayerController : MonoBehaviour {
+
+	private const string HORIZONTAL = "Horizontal";
+	private const string VERTICAL = "Vertical";
+
+	[SerializeField] private float moveSpeed = 250f;
+	[SerializeField] private float tilt;
+
+	private Rigidbody rigid;
+	private float moveHorizontal;
+	private float moveVertical;
+	private Vector3 movement;
+
+	private void Awake()
+	{
+		rigid = GetComponent<Rigidbody>();
+	}
+
+	private void FixedUpdate()
+	{
+		MoveAround();
+		PlayerBoundary.Instance.BoundaryBox(rigid);
+	}
+
+	private void MoveAround()
+	{
+		moveHorizontal = Input.GetAxis(HORIZONTAL);
+		moveVertical = Input.GetAxis(VERTICAL);
+		movement = new Vector3(moveHorizontal, 0, moveVertical);
+		rigid.velocity = movement * (moveSpeed * Time.deltaTime);
+		rigid.rotation = Quaternion.Euler(0, 0, rigid.velocity.x * -tilt);
+	}
+}
